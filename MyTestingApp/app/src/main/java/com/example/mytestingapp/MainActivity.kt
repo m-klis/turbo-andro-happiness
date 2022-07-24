f2629a968f50922cb4d8b7c46ad2d96ff2937adc
Muklis Dwi <sasantomuklis@gmail.com>
2022-07-24 22:24:10 +0700

    update testing app

--- MyTestingApp/app/src/main/java/com/example/mytestingapp/MainActivity.kt
+++ MyTestingApp/app/src/main/java/com/example/mytestingapp/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +21,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         tvText = findViewById(R.id.tv_text)
         btnSetValue = findViewById(R.id.btn_set_value)
         btnSetValue.setOnClickListener(this)
+
+        names.add("Narendra")
+        names.add("Kevin")
+        names.add("Yoza")
+        names.add("Gilang")
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.btn_set_value) {
-            tvText.text = "19"
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..3) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
         }
     }
 }
